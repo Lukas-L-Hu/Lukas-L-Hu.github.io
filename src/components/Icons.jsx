6054a0ac5efd5b5a21d652a39ec9693a23494ef0
Lukas Hu <lukashu22@gmail.com>
2025-06-25 21:32:52 -0700

    Completed Languages and Frameworks

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,17 +1,18 @@
 export default function Icons({ title, svgPath, color }) {
-    return (
-    <div className="tech-icons w-16 h-16 flex items-center justify-center" title={title}>
+  return (
+    <div className="tech-icons w-32 h-32 flex flex-col items-center justify-center" title={title}>
       <svg
         role="img"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
-        width="40"
-        height="40"
+        width="100%"
+        height="100%"
         fill={color}
       >
         <title>{title}</title>
         <path d={svgPath} />
       </svg>
+      <p className="mt-1 text-sm text-center">{title}</p>
     </div>
   );
 }
