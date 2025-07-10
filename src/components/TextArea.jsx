dd5f01de0e3c5c4247baa0fc55028f48b95a2c29
Lukas Hu <lukashu22@gmail.com>
2025-07-10 10:48:08 -0700

    Added tools and frameworks to the Home page

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -3,10 +3,10 @@ export default function TextArea({ heading, children }) {
     <div className="education-section">
       <div className="flex flex-col items-center justify-center text-center">
         <div className="heading-img-div mb-4">{/* optional image */}</div>
-        <h1 className="text-4xl font-bold text-[#5d2a42] mt-0">{heading}</h1>
+        <h1 className="text-4xl font-bold text-white mt-0">{heading}</h1>
       </div>
 
-      <div className="mt-8 p-6 rounded-xl shadow-lg bg-[#f4e2d8] max-w-4xl mx-auto text-[#5d2a42]">
+      <div className="mt-8 p-6 rounded-xl shadow-lg bg-[#f4e2d8] max-w-4xl mx-auto text-black">
         {typeof children === "string" ? (
           <div className="whitespace-pre-wrap bg-white p-6 rounded-md shadow-md text-gray-900">
             {children}
