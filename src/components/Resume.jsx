8d312786cb7c4fcb128a5d8ba983f8ba0efb0cf4
Lukas Hu <lukashu22@gmail.com>
2025-07-15 17:31:33 -0700

    Added photo on resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,12 @@
 export default function Resume() {
     return (
-        <div></div>
+        <div className="flex flex-col items-center px-4 pt-16 pb-12">
+        <img
+            src="/picture4resumepage.jpg"
+            alt="Resume Photo"
+            className="w-64 h-auto rounded-lg shadow-lg"
+        />
+        <h1 className="text-3xl font-bold mt-4">My Resume</h1>
+        </div>
     )
 }
