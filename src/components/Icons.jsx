220c03a315dce79a4a6905cc0e258d6f19cdca29
Lukas Hu <lukashu22@gmail.com>
2025-09-03 15:15:14 -0700

    Fixed Home page design for mobile

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,12 +1,11 @@
 export default function Icons({ title, svgPath, color }) {
   return (
-    <div className="tech-icons w-32 h-32 flex flex-col items-center justify-center" title={title}>
+    <div className="tech-icons flex flex-col items-center justify-center w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32" title={title}>
       <svg
         role="img"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
-        width="100%"
-        height="100%"
+        className="w-3/4 h-3/4"
         fill={color}
       >
         <title>{title}</title>
